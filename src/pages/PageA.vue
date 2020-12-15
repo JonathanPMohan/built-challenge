44dfeae67204c89cb6a9b145a0a2630507d35396
Jonathan Mohan <JonathanMohan.net@gmail.com>
2020-12-15 00:31:39 -0600

    created invoice child component for cleaner code

--- src/pages/PageA.vue
+++ src/pages/PageA.vue
@@ -2,43 +2,12 @@
   <q-page q-pa-md>
 <!-- List of Items -->
 <q-list bordered class="q-ma-md" separator>
-
-      <q-item
+      <invoice
       v-for="(invoice, key) in invoices"
       :key="key"
-      @click="invoice.paid = !invoice.paid"
-      :class="!invoice.paid ? 'bg-grey-1' : 'bg-grey-4'"
-      clickable
-      v-ripple>
-        <q-item-section side top>
-          <q-checkbox v-model="invoice.paid" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label
-          :class="{ 'text-strikethrough' : invoice.paid }">
-          {{invoice.name}}
-          </q-item-label>
-        </q-item-section>
-
-        <q-item-section side>
-          <div class="row">
-          <q-icon
-          name="event"
-          size="15px"
-          class="q-mr-sm"
-          ></q-icon>
-          <q-item-label caption>{{ invoice.dueDate }}</q-item-label>
-          </div>
-          <div class="row">
-          <small>
-          <q-icon
-          name="payment"
-          size="15px"
-          color="green"
-          class="q-mr-sm" /> <b> ${{ invoice.total}}</b></small>
-          </div>
-        </q-item-section>
-      </q-item>
+      :invoice="invoice"
+      :id="key"
+      ></invoice>
 
     </q-list>
 <!-- End of List -->
@@ -51,6 +20,9 @@ import { mapGetters } from 'vuex';
 export default {
   computed: {
     ...mapGetters('invoices', ['invoices']),
+  },
+  components: {
+    invoice: () => import('components/Invoices/Invoice.vue'),
   },
 };
 </script>
