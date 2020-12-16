40791df0ffcc81567bcd7cc63162a583790c4670
Jonathan Mohan <JonathanMohan.net@gmail.com>
2020-12-16 14:48:54 -0600

    split up paid and outstanding invoices, added list headers, added no invoices banner, simple style

---
+++ src/components/Invoices/OutstandingInvoices.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+  <list-header
+  bgColor="bg-grey-4"
+  >Outstanding Invoices</list-header>
+
+  <q-list
+    bordered
+    separator
+    class="q-ma-md">
+
+      <invoice
+      v-for="(invoice, key) in outstandingInvoices"
+      :key="key"
+      :invoice="invoice"
+      :id="key"
+      ></invoice>
+    </q-list>
+    </div>
+</template>
+
+<script>
+
+export default {
+  props: ['outstandingInvoices'],
+  components: {
+    invoice: () => import('components/Invoices/Invoice.vue'),
+    'list-header': () => import('components/Shared/ListHeader.vue'),
+  },
+};
+</script>
+    ListHeader
+
+<style lang="scss">
+
+</style>
