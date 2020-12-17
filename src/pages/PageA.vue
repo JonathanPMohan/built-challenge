960db881d17baf6200a210c41d8f0e5b5fe0944c
Jonathan Mohan <JonathanMohan.net@gmail.com>
2020-12-17 13:48:24 -0600

    added search functionality and search bar, refactor of code

--- src/pages/PageA.vue
+++ src/pages/PageA.vue
@@ -1,10 +1,24 @@
 <template>
   <q-page q-pa-md>
+
+    <div class="row">
+    <search
+    class="q-ma-md" />
+    </div>
+
+    <p class="q-pa-md" v-if="search && !Object.keys(outstandingInvoices).length
+    && !Object.keys(paidInvoices).length">
+    <q-icon name="not_interested"
+    size="25px"
+    color="red"
+    />
+    Sorry! We don't find any search results.</p>
+
     <no-invoices
-    v-if="!Object.keys(outstandingInvoices).length" />
+    v-if="!Object.keys(outstandingInvoices).length && !search" />
 <!-- List of Items -->
     <outstanding-invoices
-      v-else
+      v-if="Object.keys(outstandingInvoices).length"
       :outstandingInvoices="outstandingInvoices" />
 
     <paid-invoices
@@ -33,7 +47,7 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapState } from 'vuex';
 
 export default {
   data() {
@@ -43,12 +57,14 @@ export default {
   },
   computed: {
     ...mapGetters('invoices', ['outstandingInvoices', 'paidInvoices']),
+    ...mapState('invoices', ['search']),
   },
   components: {
     'add-invoice': () => import('components/Invoices/Modals/AddInvoice.vue'),
     'outstanding-invoices': () => import('components/Invoices/OutstandingInvoices.vue'),
     'paid-invoices': () => import('components/Invoices/PaidInvoices.vue'),
     'no-invoices': () => import('components/Invoices/NoInvoices.vue'),
+    search: () => import('components/Invoices/Tools/Search.vue'),
   },
 };
 </script>
